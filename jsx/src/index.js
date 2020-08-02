0b9859ca09bc1ab00cb94e2d9c2828d70483ead2
Abhinav Anand <abhinav.anand500@gmail.com>
2020-08-02 19:11:42 +0530

    Component

---
+++ jsx/src/index.js
@@ -0,0 +1,25 @@
+// Import React and ReactDOM
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+// Create React Component
+
+const App = ()=>{
+    const style={backgroundColor:'blue', color:'white'};
+
+    return( 
+    <div>
+        <label className="label" htmlFor="name">Enter name ::  </label>
+        <input id="name" type="text"/>
+        <button style={style}>Submit</button>
+    </div>
+    );
+};
+
+// Take React Component and show it on screen
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
