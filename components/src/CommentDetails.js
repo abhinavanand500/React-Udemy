6ae913c98ab61256e4aefdfb9158dab60ba7db34
Abhinav Anand <abhinav.anand500@gmail.com>
2020-08-04 11:48:55 +0530

    props Children

--- components/src/CommentDetails.js
+++ components/src/CommentDetails.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import faker from 'faker';
+import React from "react";
 
-const CommentDetails=(props)=>{
-    console.log(props)
-    return(
+const CommentDetails = (props) => {
+    // console.log(props);
+    return (
         <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
+            <a href="/" className="avatar">
+                <img alt="avatar" src={props.img} />
+            </a>
+            <div className="content">
+                <a href="/" className="author">
+                    {props.author}
                 </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        {props.author}
-                    </a>
-                    <div className="metadata">
-                        <span className="date">
-                            {props.timeAgo}
-                        </span>
-                    </div>
-                    <div className="text">Nice Blog Post</div>
+                <div className="metadata">
+                    <span className="date">{props.timeAgo}</span>
                 </div>
+                <div className="text">{props.content}</div>
             </div>
+        </div>
     );
-}
+};
 
 export default CommentDetails;
--- components/src/index.js
+++ components/src/index.js
@@ -1,16 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetails';
-const App=()=>{
-    return(
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetails";
+import ApprovalCard from "./ApprovalCard";
+const App = () => {
+    return (
         <div className="ui container comments">
-            <CommentDetail author="Abhinav" timeAgo="Today at 5:00 PM" />
-            <CommentDetail author="Anurag" timeAgo="Yesterday at 9:00 PM" />
-            <CommentDetail author="Ram" timeAgo="Today at 12:00 PM" />
-            <CommentDetail author="Ayush" timeAgo="Today at 3:00 PM" />
+            <ApprovalCard>
+                <CommentDetail
+                    author="Abhinav"
+                    timeAgo="Today at 5:00 PM"
+                    content="Nice"
+                    img={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Anurag"
+                    timeAgo="Yesterday at 9:00 PM"
+                    content="Good Content"
+                    img={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Ram"
+                    timeAgo="Today at 12:00 PM"
+                    content="Mast"
+                    img={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Ayush"
+                    timeAgo="Today at 3:00 PM"
+                    content="Good"
+                    img={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     );
-}
+};
 
-
-ReactDOM.render(<App/>,document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
