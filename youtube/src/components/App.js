6bf53f52061eb3c3e1eb68aed5978e7424469cda
Abhinav Anand <abhinav.anand500@gmail.com>
2020-08-08 00:12:56 +0530

    Youtube api done

---
+++ youtube/src/components/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import Youtube from "../apis/Youtube";
+class App extends React.Component {
+    state = { videos: [] };
+    onTermSubmit = async (term) => {
+        const response = await Youtube.get("/search", {
+            params: {
+                q: term,
+            },
+        });
+        console.log(response.data.items);
+        this.setState({ videos: response.data.items });
+    };
+    // API KEY YOUTUBE =====   ===   'AIzaSyCGeIt39ff3FqUI761ribO-UZLX2_p5Ef8 '
+    render() {
+        return (
+            <div className="ui container">
+                <SearchBar onFormSubmit={this.onTermSubmit} />I have{" "}
+                {this.state.videos.length} videos
+            </div>
+        );
+    }
+}
+
+export default App;
