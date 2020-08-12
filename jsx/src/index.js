db2c71a9724fbda46814b5d60355ac241fbeb5ce
Abhinav Anand <abhinav.anand500@gmail.com>
2020-08-12 15:22:09 +0530

    npm i && start

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -5,21 +5,21 @@ import ReactDOM from 'react-dom';
 
 // Create React Component
 
-const App = ()=>{
-    const style={backgroundColor:'blue', color:'white'};
+const App = () => {
+    const style = { backgroundColor: 'blue', color: 'white' };
 
-    return( 
-    <div>
-        <label className="label" htmlFor="name">Enter name ::  </label>
-        <input id="name" type="text"/>
-        <button style={style}>Submit</button>
-    </div>
+    return (
+        <div>
+            <label className="label" htmlFor="name">Enter name ::  </label>
+            <input id="name" type="text" />
+            <button style={style}>Submit</button>
+        </div>
     );
 };
 
 // Take React Component and show it on screen
 
 ReactDOM.render(
-    <App/>,
+    <App />,
     document.getElementById('root')
 );
