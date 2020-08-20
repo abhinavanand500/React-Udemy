3b50ae8bcab473575bcac12e7c4c5462545492df
Abhinav Anand <abhinav.anand500@gmail.com>
2020-08-20 18:02:38 +0530

    Goodle Auth2

---
+++ streams/client/src/components/GoogleAuth.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+    state = { isSignedIn: null }
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '426963657515-25d81358i50mlihk11b9rai7na4o7177.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+                this.auth.isSignedIn.listen(this.onAuthChange)
+            })
+        });
+    }
+
+    onSignInClick = () => {
+        this.auth.signIn();
+    }
+
+    onSignOutClick = () => {
+        this.auth.signOut();
+    }
+
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+    }
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return (
+                null
+            )
+        }
+        else if (this.state.isSignedIn) {
+            return (
+                <button className='ui red google button' onClick={this.onSignOutClick}>
+                    <i className='google icon' />Sign Out</button >
+            );
+        }
+        else {
+            return (
+                <button className='ui red google button' onClick={this.onSignInClick}>
+                    <i className='google icon' />Sign In with Google
+                </button>
+            );
+        }
+    }
+    render() {
+        return (
+            <div>{this.renderAuthButton()}</div>
+        );
+    }
+}
+
+export default GoogleAuth;
--- streams/client/src/components/Header.js
+++ streams/client/src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import GoogleAuth from './GoogleAuth';
 import { Link } from 'react-router-dom';
 const Header = () => {
     return (
-        <div className='ui secondary pointing menu'>
+        <div className='ui inverted pointing menu'>
             <Link to='/' className='item'>Stream</Link>
             <div className='right menu'>
                 <Link to='/' className='item'>All Streams</Link>
+                <GoogleAuth />
             </div>
         </div>
     )
