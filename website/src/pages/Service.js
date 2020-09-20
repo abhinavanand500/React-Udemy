747f6e98ac9bb1d6104d3aedfa70a15439143ba1
Abhinav Anand <abhinav.anand500@gmail.com>
2020-09-20 19:47:55 +0530

    Width Fixed

--- website/src/pages/Service.js
+++ website/src/pages/Service.js
@@ -23,7 +23,7 @@ const Service = () => {
 
             <div className='container-fluid mb-5'>
                 <div className='row'>
-                    <div className='col-10 mx-auto'>
+                    <div className='col-sm-12 col-md-10 mx-auto'>
                         <div className='row gy-4'>{printdata}</div>
                     </div>
                 </div>
--- website/src/pages/needed/Card.js
+++ website/src/pages/needed/Card.js
@@ -2,35 +2,33 @@ import React from "react";
 const Card = (props) => {
     console.log(props);
     return (
-        <div className="col-md-4 col-12 mx-auto">
-            <div className="card mt-3">
+        <div className='col-md-4 col-12 mx-auto'>
+            <div className='card mt-3'>
                 <img
                     src={props.imgsrc}
-                    className="card-img-top"
+                    className='card-img-top'
                     style={{ maxHeight: "210px", minHeight: "210px" }}
-                    alt="..."
+                    alt='...'
                 />
-                <div className="card-body">
+                <div className='card-body'>
                     <h5
-                        className="card-title font-weight-bold"
-                        style={{ color: "#3D856D" }}
-                    >
+                        className='card-title font-weight-bold'
+                        style={{ color: "#3D856D" }}>
                         {props.title}
                     </h5>
-                    <p className="card-text">{props.content}</p>
-                    <div className="row">
+                    <p className='card-text'>{props.content}</p>
+                    <div className='row'>
                         {props.plink ? (
-                            <div className="col-6">
+                            <div className='col-6'>
                                 <a
                                     href={props.plink}
-                                    className="btn btn-primary"
-                                >
+                                    className='btn btn-primary'>
                                     Visit Website
                                 </a>
                             </div>
                         ) : null}
-                        <div className="col-6">
-                            <a href={props.glink} className="btn btn-primary">
+                        <div className='col-6'>
+                            <a href={props.glink} className='btn btn-primary'>
                                 Get Code
                             </a>
                         </div>
