29ef05d94d9e24061082c1b895271c1b118f8082
Abhinav Anand <abhinav.anand500@gmail.com>
2020-11-02 12:17:41 +0530

    Giving Access of github link

--- website/src/pages/needed/Sdata.js
+++ website/src/pages/needed/Sdata.js
@@ -9,6 +9,7 @@ import toll from "../../images/toll.png";
 import amazon from "../../images/amazon.png";
 import netflix from "../../images/netflix.png";
 import discord from "../../images/Discord.png";
+import urlshortner from "../../images/url.png";
 
 const Sdata = [
     {
@@ -16,39 +17,46 @@ const Sdata = [
         content:
             "This is a Portfolio Website in which all details like project, resume, etc are present. You can also contact me through this website",
         title: "My Portfolio",
-        // glink:
-        //     "https://github.com/abhinavanand500/React-Udemy/tree/master/website",
+        glink:
+            "https://github.com/abhinavanand500/React-Udemy/tree/master/website",
         plink: "https://aarohan.vercel.app/",
-        glink: "/",
+        // glink: "/",
     },
     {
         imgsrc: netflix,
         content:
             "This is a clone of Netflix Website. It is created using React. We can also watch trailer for some videos by clicking on them",
         title: "Netflix Clone",
-        // glink:
-        //     "https://github.com/abhinavanand500/React-Udemy/tree/master/website",
+        glink: "https://github.com/abhinavanand500/Netflix-Clone",
         plink: "https://netflix-clone-a00f4.web.app/",
-        glink: "/",
+        // glink: "/",
     },
     {
         imgsrc: discord,
         title: "Discord Clone",
         content:
             "This is a clone of Discord. Anyone can login using Google and create or participate in any conversations. ",
-        // glink:
-        //     "https://github.com/abhinavanand500/React-Udemy/tree/master/website",
+        glink: "https://github.com/abhinavanand500/Discord-Clone",
         plink: "https://discord-clone-dcd54.web.app/",
-        glink: "/",
+        // glink: "/",
     },
     {
         imgsrc: covid,
         content:
             "It will show the details of COVID cases in whole world and each country. It is a web-App designed by using React",
         title: "COVID Tracker",
-        // glink: "https://github.com/abhinavanand500/react-covidTracker",
+        glink: "https://github.com/abhinavanand500/react-covidTracker",
         plink: "https://abhinavanand500.github.io/react-covidTracker/",
-        glink: "/",
+        // glink: "/",
+    },
+    {
+        imgsrc: urlshortner,
+        content:
+            "In this project I have developed a website which will convert your long URL into small URL.",
+        title: "URL SHORTENER",
+        glink: "https://github.com/abhinavanand500/kaleyra",
+        plink: "http://kaleyra.pythonanywhere.com/",
+        // glink: "/",
     },
 
     {
@@ -56,27 +64,27 @@ const Sdata = [
         content:
             "Employees can mark themselves present by mobile phone. And their attendance will only be marked when they are present in Office",
         title: "GeoFencing Attendance System",
-        // glink: "https://github.com/abhinavanand500/GeoFence-Django",
+        glink: "https://github.com/abhinavanand500/GeoFence-Django",
         plink: "http://abhinavanand500.pythonanywhere.com/",
-        glink: "/",
+        // glink: "/",
     },
     {
         imgsrc: sac,
         content:
             "Platform which will help the people to donate and ensure them their donation will reach to the people who actually need it. ",
         title: "SACATE",
-        // glink: "https://github.com/abhinavanand500/Sacate",
+        glink: "https://github.com/abhinavanand500/Sacate",
         plink: "http://sac.pythonanywhere.com/",
-        glink: "/",
+        // glink: "/",
     },
     {
         imgsrc: bloggify,
         content:
             "Blog created using HTML,CSS,JS, Django, SQLite. Only authorised people are allowed to comment or write blog.",
         title: "Bloggify",
-        // glink: "https://github.com/abhinavanand500/Bloggify",
+        glink: "https://github.com/abhinavanand500/Bloggify",
         plink: "http://bloggify.pythonanywhere.com/",
-        glink: "/",
+        // glink: "/",
     },
 
     {
@@ -84,19 +92,19 @@ const Sdata = [
         content:
             "This is a Mobile App which can be used to mark employees attendance and attendance will only be marked if they are near to office",
         title: "GeoFencing App",
-        // glink: "https://github.com/abhinavanand500/myAppGeofence",
+        glink: "https://github.com/abhinavanand500/myAppGeofence",
         plink:
             "https://drive.google.com/drive/folders/1p0KiUbJoFwhjd-jwhc07bPnjGpNwD29p?usp=sharing",
-        glink: "/",
+        // glink: "/v",
     },
     {
         imgsrc: amazon,
         content:
             "Amazon clone created using ReactJS. Fully functioning react web app. You can add or remove products.",
         title: "Amazon Clone",
-        // glink: "https://github.com/abhinavanand500/Bloggify",
+        glink: "https://github.com/abhinavanand500/amazon",
         plink: "https://amazon-alpha.vercel.app/",
-        glink: "/",
+        // glink: "/",
     },
 
     {
@@ -104,10 +112,10 @@ const Sdata = [
         content:
             "This is a website created using React and youtube API. It works exactly like youtube. You can watch anything",
         title: "Youtube Website Clone",
-        // glink:
-        //     "https://github.com/abhinavanand500/React-Udemy/tree/master/youtubeHooks",
+        glink:
+            "https://github.com/abhinavanand500/React-Udemy/tree/master/youtubeHooks",
         plink: "https://youtube-hooks-gilt.vercel.app/",
-        glink: "/",
+        // glink: "/",
     },
     {
         imgsrc: toll,
